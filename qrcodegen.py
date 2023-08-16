2cb4a06396b868a5aee648a072652a18e495556d
Pedro Garcia <114920145+gutybg98@users.noreply.github.com>
2023-08-16 14:07:07 +0100

    Add files via upload

---
+++ qrcodegen.py
@@ -0,0 +1,16 @@
+import os
+import qrcode
+
+link = input("Enter the URL to generate the QRCode: ")
+
+img = qrcode.make(link)
+
+if "myqrcode.png" in os.listdir():
+    counter = 1
+    file_name = "myqrcode(" + str(counter) + ").png"
+    while file_name in os.listdir():
+        counter += 1
+        file_name = "myqrcode(" + str(counter) + ").png"
+    img.save(os.getcwd() + "\\" + file_name)
+else:
+    img.save(os.getcwd() + "\\myqrcode.png")
