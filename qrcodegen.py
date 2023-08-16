6a076eaad8fc7e930d13a683cf2bda39fe03ae23
gutybg98 <pgbarrosgarcia@gmail.com>
2023-08-16 17:51:50 +0100

    melhoria da nomeclatura dos pngs

--- qrcodegen.py
+++ qrcodegen.py
@@ -6,11 +6,11 @@ link = input("Enter the URL to generate the QRCode: ")
 img = qrcode.make(link)
 
 if "myqrcode.png" in os.listdir():
-    counter = 1
-    file_name = "myqrcode(" + str(counter) + ").png"
+    counter = 0
+    file_name = "myqrcode - Copy.png"
     while file_name in os.listdir():
         counter += 1
-        file_name = "myqrcode(" + str(counter) + ").png"
+        file_name = "myqrcode - Copy (" + str(counter) + ").png"
     img.save(os.getcwd() + "/" + file_name)
 else:
     img.save(os.getcwd() + "/myqrcode.png")
